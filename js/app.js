2e4502c44d7441c0fdac4946b03865e4f1317cb7
Christopher Waite <chrismwaite@gmail.com>
2016-01-17 12:34:41 +0000

    Rendered area is now 3x3
    Added position multipliers for all axis

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 var containers = {};
 
 $(document).ready(function() {
+
+    //we need a map object
+    //a collection of rooms represents a map
+    //the map needs to define how the rooms are linked (or else the rooms do - e.g. room north, east, south, west)
+
+
     //rooms
     var room = new Room('{"top":1,"bottom":1,"left":1,"right":1,"back":0,"front":0}');
-    var room2 = new Room('{"top":1,"bottom":1,"left":1,"right":1,"back":0,"front":0}');
+    var room2 = new Room('{"top":1,"bottom":1,"left":0,"right":0,"back":0,"front":0}');
     var room3 = new Room('{"top":1,"bottom":1,"left":1,"right":1,"back":0,"front":0}');
+    var room4 = new Room('{"top":1,"bottom":1,"left":1,"right":0,"back":1,"front":1}');
+    var room5 = new Room('{"top":1,"bottom":1,"left":0,"right":1,"back":1,"front":1}');
 
     //create containers and starting rooms
-    containers['room-far'] = new Container('#room-far',0,room);
-    containers['room-middle'] = new Container('#room-middle',4,room2);
-    containers['room-near'] = new Container('#room-near',8,room3);
+    containers['room-far'] = new Container('#room-far','{"x":0,"y":0,"z":0}',room);
+    containers['room-middle-left'] = new Container('#room-middle-left','{"x":-4,"y":0,"z":4}',room4);
+    containers['room-middle'] = new Container('#room-middle','{"x":0,"y":0,"z":4}',room2);
+    containers['room-middle-right'] = new Container('#room-middle-right','{"x":4,"y":0,"z":4}',room5);
+    containers['room-near'] = new Container('#room-near','{"x":0,"y":0,"z":8}',room3);
     
     //render the room data for the containers
     for (var key in containers) {
@@ -24,10 +34,10 @@ $(document).ready(function() {
 });
 
 //Container Entity
-function Container(target, multiplier, room) {
+function Container(target, multipliers, room) {
     this.target = target;
     this.room = room;
-    this.position_multiplier = multiplier;
+    this.position_multipliers = JSON.parse(multipliers);
 }
 
 Container.prototype.render = function() {
@@ -38,42 +48,42 @@ Container.prototype.render = function() {
         if(this.room.data.top) {
             var entity = document.createElement('a-entity');
             entity.setAttribute('mixin','wall top');
-            entity.setAttribute('position','0 4 ' + this.position_multiplier);
+            entity.setAttribute('position',this.position_multipliers.x + ' 4 ' + this.position_multipliers.z);
             $(this.target).append(entity);
         }
 
         if(this.room.data.bottom) {
             var entity = document.createElement('a-entity');
             entity.setAttribute('mixin','wall bottom');
-            entity.setAttribute('position','0 0 ' + this.position_multiplier);
+            entity.setAttribute('position',this.position_multipliers.x + ' 0 ' + this.position_multipliers.z);
             $(this.target).append(entity);
         }
 
         if(this.room.data.left) {
             var entity = document.createElement('a-entity');
             entity.setAttribute('mixin','wall left');
-            entity.setAttribute('position','-2 2 ' + this.position_multiplier);
+            entity.setAttribute('position',(-2 + this.position_multipliers.x) + ' 2 ' + this.position_multipliers.z);
             $(this.target).append(entity);
         }
 
         if(this.room.data.right) {
             var entity = document.createElement('a-entity');
             entity.setAttribute('mixin','wall right');
-            entity.setAttribute('position','2 2 ' + this.position_multiplier);
+            entity.setAttribute('position',(2 + this.position_multipliers.x) + ' 2 ' + this.position_multipliers.z);
             $(this.target).append(entity);
         }
 
         if(this.room.data.back) {
             var entity = document.createElement('a-entity');
             entity.setAttribute('mixin','wall back');
-            entity.setAttribute('position','0 2 ' + (-2 + this.position_multiplier));
+            entity.setAttribute('position',this.position_multipliers.x + ' 2 ' + (-2 + this.position_multipliers.z));
             $(this.target).append(entity);
         }
 
         if(this.room.data.front) {
             var entity = document.createElement('a-entity');
             entity.setAttribute('mixin','wall front');
-            entity.setAttribute('position','0 2 ' + (2 + this.position_multiplier));
+            entity.setAttribute('position',this.position_multipliers.x + ' 2 ' + (2 + this.position_multipliers.z));
             $(this.target).append(entity);
         }
 
@@ -81,7 +91,7 @@ Container.prototype.render = function() {
         var entity = document.createElement('a-entity');
         entity.setAttribute('class','move');
         entity.setAttribute('mixin','move');
-        entity.setAttribute('position','0 1 ' + this.position_multiplier);
+        entity.setAttribute('position',this.position_multipliers.x + ' 1 ' + this.position_multipliers.z);
         $(this.target).append(entity);
 
         $(entity).click(function() {
